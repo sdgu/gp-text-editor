e6322e36f5bd51c6b64bbc5646ec0287a4213a6c
Shawn <sdgu000@gmail.com>
2016-02-08 17:55:21 -0500

    removing (red) and removing style works

--- gp.js
+++ gp.js
@@ -12,6 +12,29 @@ app.directive('ngRightClick', function($parse) {
     };
 });
 
+app.directive('sglclick', ['$parse', function($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+          var fn = $parse(attr['sglclick']);
+          var delay = 200, clicks = 0, timer = null;
+          element.on('click', function (event) {
+            clicks++;  //count clicks
+            if(clicks === 1) {
+              timer = setTimeout(function() {
+                scope.$apply(function () {
+                    fn(scope, { $event: event });
+                }); 
+                clicks = 0;             //after action performed, reset counter
+              }, delay);
+              } else {
+                clearTimeout(timer);    //prevent single-click action
+                clicks = 0;             //after action performed, reset counter
+              }
+          });
+        }
+    };
+}])
 
 app.controller("MainCtrl",
 	function($scope)
@@ -22,20 +45,35 @@ app.controller("MainCtrl",
 		// var bbArr = message.split(" ");
 		//$scope.m.dispArr = [];
 		//$scope.m.dispArr = message.split(" ");
-		$scope.m.cpArr = message.split(" ");
+		//$scope.m.cpArr = message.split(" ");
 		var htmlArr = message.split(" ");
+		var bbArr = message.split(" ");
 		//var htmlArr = $scope.m.dispArr;
 		$scope.m.dispArr = htmlArr;
+		$scope.m.cpArr = bbArr;
 
 		$scope.turnRed = function(event, index)
 		{	
 			$scope["style" + index] = {'color' : 'red', 'font-weight' : 'bold'};
 
-			$scope.m.cpArr[index] = '[B][COLOR=red]' + $scope.m.cpArr[index] + '[/COLOR][/B]';
-
+			if (!(bbArr[index].contains('[B][COLOR=red]')))
+			{
+				bbArr[index] = '[B][COLOR=red]' + bbArr[index] + '[/COLOR][/B]';
+			}
 		}
 
+		$scope.removeStyle = function(event, index)
+		{
+	
+			$scope["style" + index] = {'color' : 'none', 'font-weight' : 'none'};
 
+			if (bbArr[index].contains('[B][COLOR=red]'))
+			{
+
+				bbArr[index] = bbArr[index].substring(14);
+				bbArr[index] = bbArr[index].substring(0, bbArr[index].length - 12);
+			}
+		}
 
 
 
@@ -50,7 +88,10 @@ app.controller("MainCtrl",
 			alert(event.target.toSource());
 		}
 
-
+		$scope.test3 = function()
+		{
+			alert("2x click");
+		}
 
 
 
