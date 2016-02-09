ca5ec6ac1f546943e3b813432ab452e25f8294ee
Shawn <sdgu000@gmail.com>
2016-02-08 21:44:46 -0500

    added limit so it loads faster

--- gp.js
+++ gp.js
@@ -38,38 +38,36 @@ app.directive('sglclick', ['$parse', function($parse) {
 
 
 
+
 app.controller("MainCtrl",
 	function($scope)
 	{
 	
 
+		// var message = "pasta apple";
+		// var $scope.m.dispArr = message.split(" ");
+		// var $scope.m.cpArr = message.split(" ");
 		
-		var message = $scope.thingToGP;
-		//var message = $scope.thingToGP;
-		//"I like pasta and Smogon drools a lot. Also Smogon is cyka.";
+		// $scope.m.dispArr = $scope.m.dispArr;
+		// $scope.m.cpArr = $scope.m.cpArr;
 		
-		// var bbArr = message.split(" ");
-		//$scope.m.dispArr = [];
-		//$scope.m.dispArr = message.split(" ");
-		//$scope.m.cpArr = message.split(" ");
+		$scope.limit = 200;
+		$scope.go = function()
+		{
+			$scope.m.dispArr = $scope.thingToGP.replace(/\n/g, "<br>").split(" ");
+			$scope.m.cpArr = $scope.thingToGP.replace(/\n/g, "<br>").split(" ");
+		}
 
+		$scope.copyToBoard = function()
+		{
+			$scope.cpthis = $scope.m.cpArr.join(" ");
 
-		//var message = "dong masters"
-	
+		}
 
-		var htmlArr = message.split(" ");
-		var bbArr = message.split(" ");
-		
-
-
-		//var htmlArr = $scope.m.dispArr;
-		$scope.m.dispArr = htmlArr;
-		$scope.m.cpArr = bbArr;
-	
-
-
-
-
+		$scope.increaseLimit = function()
+		{
+			$scope.limit += 200;
+		}
 		var firstInd = 0;
 		var secondInd = 0;
 		var shiftCount = 0;
@@ -77,6 +75,8 @@ app.controller("MainCtrl",
 		$scope.resetShift = function()
 		{
 			shiftCount = 0;
+
+			
 		}
 
 		$scope.turnRed = function(event, index)
@@ -99,10 +99,10 @@ app.controller("MainCtrl",
 				{
 					for (var i = Math.min(firstInd, secondInd); i < Math.max(firstInd, secondInd) + 1; i++)
 					{
-						htmlArr[i] = '<strong class="redRemove">' + htmlArr[i] + '</strong>';
-						if (!(bbArr[i].contains('[B][COLOR=red]')))
+						$scope.m.dispArr[i] = '<strong class="redRemove">' + $scope.m.dispArr[i] + '</strong>';
+						if (!($scope.m.cpArr[i].contains('[B][COLOR=red]')))
 						{
-							bbArr[i] = '[B][COLOR=red]' + bbArr[i] + '[/COLOR][/B]';
+							$scope.m.cpArr[i] = '[B][COLOR=red]' + $scope.m.cpArr[i] + '[/COLOR][/B]';
 						}
 					}
 					shiftCount = 0;
@@ -117,11 +117,11 @@ app.controller("MainCtrl",
 
 
 				//$scope["style" + index] = {'color' : 'red', 'font-weight' : 'bold'};
-				htmlArr[index] = '<strong class="redRemove">' + htmlArr[index] + '</strong>';
+				$scope.m.dispArr[index] = '<strong class="redRemove">' + $scope.m.dispArr[index] + '</strong>';
 				//only need to check the beginning because removing will remove both sides
-				if (!(bbArr[index].contains('[B][COLOR=red]')))
+				if (!($scope.m.cpArr[index].contains('[B][COLOR=red]')))
 				{
-					bbArr[index] = '[B][COLOR=red]' + bbArr[index] + '[/COLOR][/B]';
+					$scope.m.cpArr[index] = '[B][COLOR=red]' + $scope.m.cpArr[index] + '[/COLOR][/B]';
 				}
 			}
 		}
@@ -148,19 +148,19 @@ app.controller("MainCtrl",
 					{
 
 						$scope["style" + i] = {'color' : 'none', 'font-weight' : 'none'};
-						if (bbArr[i].contains('[B][COLOR=red]'))
+						if ($scope.m.cpArr[i].contains('[B][COLOR=red]'))
 						{
 
-							bbArr[i] = bbArr[i].substring(14);
-							bbArr[i] = bbArr[i].substring(0, bbArr[i].length - 12);
+							$scope.m.cpArr[i] = $scope.m.cpArr[i].substring(14);
+							$scope.m.cpArr[i] = $scope.m.cpArr[i].substring(0, $scope.m.cpArr[i].length - 12);
 						}
 
-						//if (bbArr[i].contains('[B][COLOR=blue]'))
+						//if ($scope.m.cpArr[i].contains('[B][COLOR=blue]'))
 						// {
-						// 	bbArr.splice(i, Math.max(firstInd, secondInd) - Math.min(firstInd, secondInd) + 1);
-						// 	htmlArr.splice(i, Math.max(firstInd, secondInd) - Math.min(firstInd, secondInd) + 1);
-						// 	// bbArr[index] = bbArr[index].substring(16);
-						// 	// bbArr[index] = bbArr[index].substring(0, bbArr[index].length - 12);
+						// 	$scope.m.cpArr.splice(i, Math.max(firstInd, secondInd) - Math.min(firstInd, secondInd) + 1);
+						// 	$scope.m.dispArr.splice(i, Math.max(firstInd, secondInd) - Math.min(firstInd, secondInd) + 1);
+						// 	// $scope.m.cpArr[index] = $scope.m.cpArr[index].substring(16);
+						// 	// $scope.m.cpArr[index] = $scope.m.cpArr[index].substring(0, $scope.m.cpArr[index].length - 12);
 						// }
 
 					}
@@ -173,28 +173,28 @@ app.controller("MainCtrl",
 			}
 	
 			//$scope["style" + index] = {'color' : 'none', 'font-weight' : 'none'};
-			if (htmlArr[index].contains('<strong class="redRemove">'))
+			if ($scope.m.dispArr[index].contains('<strong class="redRemove">'))
 			{
-				htmlArr[index] = htmlArr[index].substring(26);
-				htmlArr[index] = htmlArr[index].substring(0, htmlArr[index].length - 9);
+				$scope.m.dispArr[index] = $scope.m.dispArr[index].substring(26);
+				$scope.m.dispArr[index] = $scope.m.dispArr[index].substring(0, $scope.m.dispArr[index].length - 9);
 			}
 
-			if (bbArr[index].contains('[B][COLOR=red]'))
+			if ($scope.m.cpArr[index].contains('[B][COLOR=red]'))
 			{
-				bbArr[index] = bbArr[index].substring(14);
-				bbArr[index] = bbArr[index].substring(0, bbArr[index].length - 12);
+				$scope.m.cpArr[index] = $scope.m.cpArr[index].substring(14);
+				$scope.m.cpArr[index] = $scope.m.cpArr[index].substring(0, $scope.m.cpArr[index].length - 12);
 			}
 
-			if (bbArr[index].contains('[B][COLOR=blue]'))
+			if ($scope.m.cpArr[index].contains('[B][COLOR=blue]'))
 			{
-				bbArr.splice(index, 1);
-				htmlArr.splice(index, 1);
+				$scope.m.cpArr.splice(index, 1);
+				$scope.m.dispArr.splice(index, 1);
 			
 				$scope["style" + (index)] = {'color' : 'none', 'font-weight' : 'none'};
 				$scope["style" + (index-1)] = {'color' : 'none', 'font-weight' : 'none'};
 
-				// bbArr[index] = bbArr[index].substring(16);
-				// bbArr[index] = bbArr[index].substring(0, bbArr[index].length - 12);
+				// $scope.m.cpArr[index] = $scope.m.cpArr[index].substring(16);
+				// $scope.m.cpArr[index] = $scope.m.cpArr[index].substring(0, $scope.m.cpArr[index].length - 12);
 			}
 			// else
 			// {
@@ -223,15 +223,15 @@ app.controller("MainCtrl",
 
 			if (event.keyCode === 13)
 			{
-				//htmlArr.splice(index + 1, 0, arr2add);
+				//$scope.m.dispArr.splice(index + 1, 0, arr2add);
 				
 				for (var i = index; i < index + arr2add.length; i++)
 				{
 					
-					htmlArr.splice(i + 1, 0, arr2add[i-index]);
-					htmlArr[i + 1] = '<strong class="blueAdd">' + htmlArr[i + 1] + "</strong>";
+					$scope.m.dispArr.splice(i + 1, 0, arr2add[i-index]);
+					$scope.m.dispArr[i + 1] = '<strong class="blueAdd">' + $scope.m.dispArr[i + 1] + "</strong>";
 					//$scope["style" + (i+1)] = {'color' : 'blue', 'font-weight' : 'bold'};
-					bbArr.splice(i + 1, 0, '[B][COLOR=blue]' + arr2add[i-index] + '[/COLOR][/B]');
+					$scope.m.cpArr.splice(i + 1, 0, '[B][COLOR=blue]' + arr2add[i-index] + '[/COLOR][/B]');
 
 				}
 
@@ -262,3 +262,4 @@ app.controller("MainCtrl",
 
 
 	});
+
